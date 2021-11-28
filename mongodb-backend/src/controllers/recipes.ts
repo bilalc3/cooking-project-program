6a7bbf4069f54886813aa8f7d49e6c7ce8a9ddd6
MoonArchitect <45274732+MoonArchitect@users.noreply.github.com>
2021-11-27 22:38:30 -0500

    Improve search functionality with Atlas Search

--- mongodb-backend/src/controllers/recipes.ts
+++ mongodb-backend/src/controllers/recipes.ts
@@ -7,11 +7,11 @@ export const getRecipe: RequestHandler = (req, res) => {
     
     // TODO remove duplicate code
     if (title == null)
-        return res.status(400).json("Error: 'title' was not passed")
+        return res.status(400).json("Error: 'title' was not passed");
     else if (typeof title !== 'string' && !(title instanceof String))   // if not a string 
-        return res.status(400).json(`Error: 'title' should be a single string value, recieved ${typeof title}`)
+        return res.status(400).json(`Error: 'title' should be a single string value, recieved ${typeof title}`);
     else
-        title = title.toString()
+        title = title.toString();
 
     
     Recipe.find({'title': title})
@@ -20,35 +20,67 @@ export const getRecipe: RequestHandler = (req, res) => {
 };
 
 
+
 export const searchRecipe: RequestHandler = (req, res) => {
     let title = req.query.title;
-    let limit: number = Number(req.query.limit) || 0;
+    let limit: number = Number(req.query.limit) || 100000;
+    let skip: number = Number(req.query.skip) || 0;
     
     if (title == null)
-        return res.status(400).json("Error: 'title' was not passed")
+        return res.status(400).json("Error: 'title' was not passed");
     else if (typeof title !== 'string' && !(title instanceof String))   // if not a string 
-        return res.status(400).json(`Error: 'title' should be a single string value, recieved ${typeof title}`)
+        return res.status(400).json(`Error: 'title' should be a single string value, recieved ${typeof title}`);
     else
-        title = title.toString()
+        title = title.toString();
 
-    
-    Recipe.find({$text: {$search: title}})
-        .select(['title', 'total_price', 'source', 'description', 'preview_image_url'])
-        .sort( { score: { $meta: "textScore" } } )
-        .limit(limit)
+    const pipeline = [
+        {
+            $search: {
+                "index": "autocompleteIndex",
+                "autocomplete": {
+                  "query": title,
+                  "path": "title",
+                //   "fuzzy": {}
+                },
+                // "highlight": {
+                //   "path": "title"
+                // }
+            }
+        },
+
+        { $skip: skip },
+        { $limit: limit },
+        
+        {
+            $project: {
+                'title': 1, 
+                'total_price': 1, 
+                'source': 1, 
+                'description': 1, 
+                'preview_image_url': 1,
+                "score": { "$meta": "searchScore" },
+                // "highlights": { "$meta": "searchHighlights" },
+            }
+        }
+    ];
+
+    Recipe.aggregate(pipeline)
         .then(recipes => res.json(recipes))
-        .catch(err => res.status(400).json('Error ' + err));
+        .catch(err => res.status(400).json(`Error '${err}'`));
 };
+
 
 
 export const getRecipesInPriceRange: RequestHandler = (req, res) => {
     let minPrice: number = Number(req.query.minPrice) || 0;
     let maxPrice: number = Number(req.query.maxPrice) || 100000000;
     let limit: number = Number(req.query.limit) || 0;
+    let skip: number = Number(req.query.skip) || 0;
 
     
     Recipe.find({ total_price: { $lte: maxPrice, $gte: minPrice } })
         .select(['title', 'total_price', 'source', 'description', 'preview_image_url'])
+        .skip(skip)
         .limit(limit)
         .then(recipes => res.json(recipes))
         .catch(err => res.status(400).json('Error message: ' + err));
